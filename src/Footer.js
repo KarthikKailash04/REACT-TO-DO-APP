7aee91e38605fef23dc516be00fbfef8b5b1ceb4
KarthikKailash04 <128879871+KarthikKailash04@users.noreply.github.com>
2023-08-27 15:32:53 +0530

    Delete src directory

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-    const dat = new Date();
-  return (
-<footer><h2> COPYTIGHT  &copy; {dat.getFullYear()}</h2>
-</footer>    )
-}
-export default Footer;
